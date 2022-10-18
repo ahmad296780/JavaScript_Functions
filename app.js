56b676de5951c33cd77b429e40e434c2c592fc65
ahmad <bha296780@gmail.com>
2022-10-18 16:40:02 -0400

    final

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
-console.log("Hello World!\n==========\n");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+// let count = (100)
+// function printOdds(count) {
+//     for (let i = 0; i < count; i++) {
 
-// Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+//         if (i % 2 != 0) {
+//             console.log(i)
+//         } else {
+//             if (count < 0)
+//                 console.log("use a positive number")
+//         }
+//     }
+// }
+// printOdds(100);
+// // exercise
+
+// function greeter(userName, age) {
+//     let aboveSixteen = `Congrats ${userName}, you can drive!`;
+//     let belowSixteen=`Sorry ${userName},you need to wait`;
+//     if (age > 16) {
+//         console.log(aboveSixteen)
+//     } else {if (age<16) {
+//         console.log(belowSixteen);
+//     }
+
+//     }
+// }
+// greeter("ahmad",24);
+// function checkValidity(a, b, c)
+// {
+    
+//     if (a + b <= c || a + c <= b || b + c <= a)
+//         return false;
+//     else
+//         return true;
+// }
+
+// let a = 7, b = 10, c = 5;
+// if (checkValidity(a, b, c))
+// document.write("Valid");
+// else
+// document.write("Invalid"); 
